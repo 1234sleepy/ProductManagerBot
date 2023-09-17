1773f64a15c7da7aada6bf1ffeba5b31b29db418
AkumaOff <122884256+AkumaOff@users.noreply.github.com>
2023-09-17 10:52:50 -0800

    Entity2

--- MarketBot/Data/Entities/Manufacture.cs
+++ MarketBot/Data/Entities/Manufacture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace ProductManagerBot.Data.Entities
     public class Manufacture : IEntity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string? Name { get; set; }
     }
 }
--- MarketBot/Data/Entities/Product.cs
+++ MarketBot/Data/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,21 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ProductManagerBot.Data.Entities
 {
-    public class Product : IEntity
+    public class Product : AddedDateEntity, IEntity
     {
         public int Id { get; set; }
-        public int Barcode { get; set; }
+        public string? Barcode { get; set; }
         public double Weight { get; set; }
-        public double Calories { get; set; }
-        public string DateOfManufacture  { get; set; }
-        public string DateOfUse { get; set; }
-        public string ProductContent{ get; set; }
+        public double? Calories { get; set; }
+        public DateTime DateOfManufacture  { get; set; }
+        public DateTime DateOfUse { get; set; }
+        public string? ProductContent { get; set; }
         public int ManufacturerId { get; set; }
-        public string Name{ get; set; }
-        public DateTime AddedDate{ get; set; }
-        public int UsweId{ get; set; }
-        public int CategoryId{ get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string? Name { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+
     }
 }
