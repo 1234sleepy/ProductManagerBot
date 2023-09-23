445e170d139c9491fe092667ee5ee7019d08f432
AkumaOff <122884256+AkumaOff@users.noreply.github.com>
2023-09-23 10:13:27 -0800

    DbContext

--- MarketBot/Program.cs
+++ MarketBot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using _RegisterBot;
+using Microsoft.EntityFrameworkCore;
+using ProductManagerBot.Data.Entities;
 
 string token = "";
 
@@ -7,3 +9,23 @@ RegisterBot bot = new RegisterBot(token);
 bot.Start();
 bot.GetStatus();
 Console.ReadLine();
+
+internal class AppDbContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<FavoriteProduct> FavoriteProducts => Set<FavoriteProduct>();
+    public DbSet<Manufacture> Manufactures => Set<Manufacture>();
+    public DbSet<Product> Products => Set<Product>();
+
+    public AppDbContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source = medb.db");
+    }
+}
