db05df92cde9e66ec08ca6142e3ac5b1dffc004c
maxpe3447 <mpechersky16@gmail.com>
2024-01-06 20:30:40 +0200

    update cookies

--- MarketBot/Services/LookupService/LookupService.cs
+++ MarketBot/Services/LookupService/LookupService.cs
@@ -1,5 +1,4 @@
 ﻿using ProductManagerBot.Data.Entities;
-using System.IO;
 using System.Net;
 
 namespace ProductManagerBot.Services.LookupService
@@ -16,26 +15,32 @@ namespace ProductManagerBot.Services.LookupService
             using HttpClient client = new HttpClient(handler);
             try
             {
-                if (File.Exists("cookie.txt"))
-                {
-                    File.ReadAllText("cookie.txt")
-                        .Split('\n')
-                        .Select(x =>
-                        {
-                            var cookieData = x.Split("=");
-                            return new Cookie(cookieData[0], cookieData[1], "/", "barcodelookup.com");
-                        }).ToList().ForEach(coockie => cookieContainer.Add(coockie));
+                //if (File.Exists("cookie.txt"))
+                //{
+                //    File.ReadAllText("cookie.txt")
+                //        .Split('\n')
+                //        .Select(x =>
+                //        {
+                //            var cookieData = x.Split("=");
+                //            return new Cookie(cookieData[0], cookieData[1], "/", "barcodelookup.com");
+                //        }).ToList().ForEach(coockie => cookieContainer.Add(coockie));
 
-                }
-                else
+                //}
+                //else
                 {
-                    cookieContainer.Add(new Cookie("_ga_6K9HJQ9YDK", "GS1.1.1703444719.4.1.1703444724.0.0.0", "/", ".barcodelookup.com"));
-                    cookieContainer.Add(new Cookie("cf_clearance", "WGbxCfKU3EKy_3KnUdsqLe184_O8C5xG3P6lKQRl_bU-1703444721-0-2-7c3054b2.3e1d2dff.66da7a02-0.2.1703444721", "/", ".barcodelookup.com"));
-                    cookieContainer.Add(new Cookie("bl_csrf", "1e2f6cb61e00c023b46115d4b514d19f", "/Session", ".barcodelookup.com"));
-                    cookieContainer.Add(new Cookie("__cflb", "04dToRCegghj9KSg7BqsUc4efEezbNiXuGJqGGCxbP", "/", "www.barcodelookup.com"));
-                    cookieContainer.Add(new Cookie("bl_session", "6a80ob7qdaefmk95f67u9rbb12kimvff", "/", ".barcodelookup.com"));
+                    cookieContainer.Add(new Cookie("cf_clearance", "cJGeMmjAjtcet.N_1cffsiurhYaoGI4mGVE6fP2b4J0-1703447927-0-2-7c3054b2.3e1d2dff.66da7a02-0.2.1703447927", "/", ".barcodelookup.com"));
+
+                    cookieContainer.Add(new Cookie("_ga_6K9HJQ9YDK", "GS1.1.1703447925.5.1.1703447926.0.0.0", "/", ".barcodelookup.com"));
+
+                    cookieContainer.Add(new Cookie("bl_csrf", "1e2f6cb61e00c023b46115d4b514d19f", "/", ".barcodelookup.com"));
+
+                    cookieContainer.Add(new Cookie("__cflb", "04dToRCegghj9KSg7BqsUc4efEezbNiZ67xwfzK8JR", "/", "www.barcodelookup.com"));
+
+                    cookieContainer.Add(new Cookie("bl_session", "d8tgf1mgicne8d93tmbbkm8am43ef2bt", "/", ".barcodelookup.com"));
+
                     cookieContainer.Add(new Cookie("_ga", "GA1.1.859256291.1702841831", "/", ".barcodelookup.com"));
-                    cookieContainer.Add(new Cookie("__cf_bm", "RJfeQpXfqvtTnVPSlChwxipjXJNX8.R8g.v.5.oOVEM-1703444719-1-AdNVe68550TYzrDXBpevak8N1LTwmKBNK49/jwFAwCdHLMoYWduMSuPBL5DmoiGlJXZN2Dz6peeVMfxUFfFZTg96kdkvP3OyzN6m3k3LBFBn", "/", ".barcodelookup.com"));
+
+                    cookieContainer.Add(new Cookie("__cf_bm", "wqeR_lRpzUYwCwTZ9LqmcCxOICsiPhiJ1PM22Q5PfU4-1703447926-1-ATiCnNBoRcaDMFhNdbG18pOgSu0VuC93M8u/QhhfYLLrho8DF5EtdemU387F5TKe74Va7DxUSUQ8Db/IfwdV1xOs829Awzq/qoFsixaGDFTu", "/", ".barcodelookup.com"));
                 }
                 HttpRequestMessage request = new(HttpMethod.Get, $"https://www.barcodelookup.com/{barcode}");
                 request.Headers.Add("Accept-Language", "en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7,uk;q=0.6");
@@ -66,13 +71,14 @@ namespace ProductManagerBot.Services.LookupService
                 int indexOfEditButton = page.IndexOf("edit-product-btn");
                 int startIndex = page.IndexOf("<h4>", indexOfEditButton);
                 int endIndex = page.IndexOf("</h4>", startIndex);
-                string name = page.Substring(startIndex+4, endIndex - startIndex-4).Trim();
+                string name = page.Substring(startIndex + 4, endIndex - startIndex - 4).Trim();
 
 
                 int manufOfEditButton = page.IndexOf("Manufacturer");
                 int startmanuf = page.IndexOf("Manufacturer:", manufOfEditButton);
                 int endmanuf = page.IndexOf("Manufacturer:", startIndex);
-                string Manufacturer = page.Substring(startmanuf + 28, endmanuf - startmanuf - 7).Trim();
+                var manufacturer = new Manufacture { Name = page.Substring(startmanuf + 28, endmanuf - startmanuf - 7).Trim() };
+
 
                 int kcalOfEditButton = page.IndexOf("Manufacturer");
                 int kcal = 0;
@@ -87,21 +93,26 @@ namespace ProductManagerBot.Services.LookupService
                 int indexOfBar = page.IndexOf(" Barcode Formats:");
                 startIndex = page.IndexOf("<span>", indexOfEditButton);
                 endIndex = page.IndexOf("</span>", startIndex);
-                string barcoder = page.Substring(startIndex + 27, endIndex - startIndex - 7).Trim();
+                string barcoder = page.Substring(startIndex + 7, endIndex - startIndex - 7).Trim();
 
                 int indexOfCategory = page.IndexOf("Category:");
 
                 Category category = null;
-                //string categor = "";
                 if (indexOfCategory > 0)
                 {
                     startIndex = page.IndexOf("<span>", indexOfEditButton);
                     endIndex = page.IndexOf("</span>", startIndex);
-                    category = new Category{ Name = page.Substring(startIndex + 27, endIndex - startIndex - 7).Trim() };
+                    category = new Category { Name = page.Substring(startIndex + 7, endIndex - startIndex - 7).Trim() };
                 }
 
-
-                return new Product { Name = name, Barcode = barcoder, Category = new Category { Name = categor },Manufacture = new Manufacture {Name = Manufacturer }, Calories = kcal};
+                return new Product 
+                { 
+                    Name = name, 
+                    Barcode = barcoder, 
+                    Category = category, 
+                    Manufacture = manufacturer, 
+                    Calories = kcal 
+                };
 
             }
             catch
